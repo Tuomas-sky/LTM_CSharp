0c422ed7c32eb00f000361288f04be82ece09a27
tuomas <1849843827@qq.com>
2025-12-08 18:38:15 +0800

    add Trigger相关操作

---
+++ WPF_Story_08_TriggerAndStyle/Age2Check_Conventer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_Story_08_TriggerAndStyle
+{
+    public class Age2Check_Conventer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int age = (int)value;
+            return age <= 35 ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
